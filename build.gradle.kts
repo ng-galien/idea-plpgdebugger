0971cda9eeb32fce3ab2fc4bbbd20ec12f0f7546
Alexandre Boyer <nggalien@gmail.com>
2023-02-01 16:15:45 +0100

    Update plugin and kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     // Java support
-    id("java")
+    java
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.12.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
@@ -55,7 +55,7 @@ dependencies {
 
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(properties("javaVersion")))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
